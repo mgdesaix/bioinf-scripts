72960ee8e0752bc317bf48af631ab656b171fb2b
Matthew Desaix <mgdesaix@Matthews-iMac.local>
2020-04-08 18:45:58 -0600

    added stuff

---
+++ additional/create-new-list.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+touch new-lists.txt
+
+for err in *.err;
+do 
+	if grep -q 'CANCELLED' ${err} 
+	then
+	  list=$(sed -n -e 's/^.*list=//p' ${err})
+	  list_dir=$(echo ${list} | cut -d'/' -f1-8)
+	  list_file=$(echo ${list} | cut -d'/' -f9 | cut -d'.' -f1-3)
+	  new_list=$(echo ${list_dir}/${list_file}x1.list)
+	  
+		vcf=$(sed -n -e 's/^.*ofile=//p' ${err})
+		vcf_dir=$(echo ${vcf} | cut -d'/' -f1-7)
+		vcf_file=$(echo ${vcf} | cut -d'/' -f8)
+		
+		last=$(zcat ${vcf_dir}/clean.${vcf_file} | tail -n 1 | awk '{print $1 ":" $2}')
+		SCAFF=$(echo ${last} | awk -F ":" '{print $1}')
+		POS=$(echo $(($(echo ${drop_last} | awk -F ":" '{print $2}')+1)))
+		LENGTH=$(echo ${SCAFF} | awk -F "size" '{print $2}')
+		
+		awk -v scaff=$SCAFF -v pos=$POS -v len=$LENGTH '$1==scaff {print scaff ":" pos "-" len; go=1; next} go==1 {print}' $(echo ${list}) > ${new_list}
+		echo ${vcf} ${list} ${new_list} >> new-lists.txt
+	fi
+done
--- additional/drop-last.sh
+++ additional/drop-last.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+infile=incomplete-vcfs.txt
+touch vcf-last-line.txt
+
+for i in {2..140}
+do
+  # get specific line from infile, which is absolute path to vcf
+  vcf=`sed -n ${i}p ${infile}`
+  drop_last=$(zcat ${vcf} | tail -n 1 | awk '{print $1 ":" $2}')
+  vcf_dir=`echo ${vcf} | cut -d'/' -f1-7`
+  vcf_file=`echo ${vcf} | cut -d'/' -f8`
+  bcftools view -Oz -t ^${drop_last} ${vcf} > ${vcf_dir}/clean.${vcf_file}
+  echo ${drop_last} >> vcf-last-line.txt
+done
